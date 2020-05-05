690de796556f3f835a7e7a4dbcdaa2c59d78b728
Billzzhang <billzhang345@gmail.com>
2020-05-05 17:01:00 -0400

    added rspec

--- app/controllers/meeting_controller.rb
+++ app/controllers/meeting_controller.rb
@@ -29,17 +29,17 @@ class MeetingController < ApplicationController
         id = Digest::MD5.hexdigest(Time.now.to_s)
 
         # Creating the meeting
-        createcall = "createname=#{id}&meetingID=#{id}"+SECRET
+        createcall = "createname=#{id}&meetingID=#{id}&record=true"+SECRET
         createsha1 = Digest::SHA1.hexdigest createcall
-        createLink = SERVER+"/create?name=#{id}&meetingID=#{id}&checksum="+createsha1
+        createLink = SERVER+"/create?name=#{id}&meetingID=#{id}&record=true&checksum="+createsha1
         response = HTTParty.get(createLink)
         createInfo = Nokogiri::XML(response.body)
-        attendeePW = createInfo.at_xpath("//attendeePW").text()
+        moderatorPW = createInfo.at_xpath("//moderatorPW").text()
 
         # Join the meeting that was just created
-        joincall = "joinfullName=#{params[:name]}&meetingID=#{id}&password=#{attendeePW}"+SECRET
+        joincall = "joinfullName=#{params[:name]}&meetingID=#{id}&password=#{moderatorPW}"+SECRET
         joinsha1 = Digest::SHA1.hexdigest joincall
-        joinLink = SERVER+"/join?fullName=#{params[:name]}&meetingID=#{id}&password=#{attendeePW}&checksum="+joinsha1
+        joinLink = SERVER+"/join?fullName=#{params[:name]}&meetingID=#{id}&password=#{moderatorPW}&checksum="+joinsha1
         redirect_to joinLink
     end
 
