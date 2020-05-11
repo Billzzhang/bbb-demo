6329ae481cb9b7a5d0059410487b3e2c8a08bd44
Billzzhang <billzhang345@gmail.com>
2020-05-11 15:57:50 -0400

    Added users and meeting relation

--- app/controllers/meeting_controller.rb
+++ app/controllers/meeting_controller.rb
@@ -2,14 +2,10 @@ require 'digest'
 require 'httparty'
 require 'nokogiri'
 class MeetingController < ApplicationController
-
-    SERVER = "https://bbb.bill.blindside-dev.com/bigbluebutton/api"
-    SECRET = "I1srupllZAut4ZpwrJwV5CDZ4nLwy7VsYE3EaBg"
-
     def home
-        getRecordingCall = "getRecordings"+SECRET
+        getRecordingCall = "getRecordings"+ENV['SECRET']
         getRecordingsha1 = Digest::SHA1.hexdigest getRecordingCall
-        getRecordingLink = SERVER+"/getRecordings?checksum="+getRecordingsha1
+        getRecordingLink = ENV['SERVER']+"/getRecordings?checksum="+getRecordingsha1
         response = HTTParty.get(getRecordingLink)
         recordingsXML = Nokogiri::XML(response.body).xpath("//recording")
         @recordings = []
@@ -29,25 +25,25 @@ class MeetingController < ApplicationController
         id = Digest::MD5.hexdigest(Time.now.to_s)
 
         # Creating the meeting
-        createcall = "createname=#{id}&meetingID=#{id}&record=true"+SECRET
+        createcall = "createname=#{id}&meetingID=#{id}&record=true"+ENV['SECRET']
         createsha1 = Digest::SHA1.hexdigest createcall
-        createLink = SERVER+"/create?name=#{id}&meetingID=#{id}&record=true&checksum="+createsha1
+        createLink = ENV['SERVER']+"/create?name=#{id}&meetingID=#{id}&record=true&checksum="+createsha1
         response = HTTParty.get(createLink)
         createInfo = Nokogiri::XML(response.body)
         moderatorPW = createInfo.at_xpath("//moderatorPW").text()
 
         # Join the meeting that was just created
-        joincall = "joinfullName=#{params[:name]}&meetingID=#{id}&password=#{moderatorPW}"+SECRET
+        joincall = "joinfullName=#{params[:name]}&meetingID=#{id}&password=#{moderatorPW}"+ENV['SECRET']
         joinsha1 = Digest::SHA1.hexdigest joincall
-        joinLink = SERVER+"/join?fullName=#{params[:name]}&meetingID=#{id}&password=#{moderatorPW}&checksum="+joinsha1
+        joinLink = ENV['SERVER']+"/join?fullName=#{params[:name]}&meetingID=#{id}&password=#{moderatorPW}&checksum="+joinsha1
         redirect_to joinLink
     end
 
     def delete
         deleteIDs = params[:id].join(",")
-        deletecall = "deleteRecordingsrecordID=#{deleteIDs}"+SECRET
+        deletecall = "deleteRecordingsrecordID=#{deleteIDs}"+ENV['SECRET']
         deletesha1 = Digest::SHA1.hexdigest deletecall
-        deleteLink = SERVER+"/deleteRecordings?recordID=#{deleteIDs}&checksum="+deletesha1
+        deleteLink = ENV['SERVER']+"/deleteRecordings?recordID=#{deleteIDs}&checksum="+deletesha1
         response = HTTParty.get(deleteLink)
         redirect_to "/"
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_05_11_195456) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "meetings", force: :cascade do |t|
+    t.string "meeting_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
